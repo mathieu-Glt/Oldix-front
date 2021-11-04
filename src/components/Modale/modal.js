bafa9067a8f9596861b14e82ad8ef36a46685b12
Thomas Monnet <thomas.monnet.9@gmail.com>
2021-11-04 16:03:10 +0100

    for help on issue

--- src/components/Modale/modal.js
+++ src/components/Modale/modal.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 import './modal.scss';
+// const [show, setShow] = useState(false);
+// export function handleClose () {setShow(false)}
+// export function handleShow () {setShow(true)}
 
 const Modal = ({ showModal, children, closeModal }) => {
-  
+
   return (
     showModal && (
       <div className="modal" onClick = { closeModal }>       
